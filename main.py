8a7ec77536aecbc64ccbd55415905235ec199ac2
SaeedZahran <saeedzahran@hotmail.com>
2022-11-09 11:09:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import mne
 import json
 from mne.preprocessing import ICA
 
+#workaround for -- _tkinter.TclError: invalid command name ".!canvas"
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
 with open('config.json') as config_json:
     config = json.load(config_json)
 
@@ -16,3 +22,7 @@ fname = config['ica']
 ica=mne.preprocessing.read_ica(fname, verbose=None)
 
 ica.apply(raw)
+
+plt.figure(1)
+ica.plot_overlay(raw, exclude=[3], picks='mag')
+plt.savefig(os.path.join('out_figs','plot_overlay.png'))
